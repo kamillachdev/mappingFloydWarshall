d53bdb474cdb68d5fecbc607ff3ddf4f0eba395b
kamillachdev <kamillachdev@gmail.com>
2023-09-24 16:09:44 +0200

    added two buttons: reset and algorithm start, added onCreate alert with program usage instructions

--- app/src/main/java/com/example/mappingfloydwarshall/MainActivity.kt
+++ app/src/main/java/com/example/mappingfloydwarshall/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mappingfloydwarshall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("OBSŁUGA PROGRAMU")
+        builder.setMessage("1. Naciśnij jeden z obrazków\n2. Wybierz wagę za pomocą slider'a\n3. Naciśnij inny obrazek, który jest połączony strzałką z poprzednio klikniętym obrazkiem\n4. Powtarzaj poprzednie 3 kroki aby ustalić wiecej wag(opcjonalne)\n5. Naciśnij przycisk 'WYZNACZ TRASĘ' aby wyznaczyć najkrótszą trasę - podświetli się ona na zielono\n6. Naciśnij przycisk 'RESET WAGI' aby wyzerować wszystkie ustawione wagi(opcjonalne)")
+        builder.setCancelable(false)
+        builder.setPositiveButton("OK") {
+            dialog, which -> dialog.cancel()
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
