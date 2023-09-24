407b29bc6f07310169d4eb72edf5cbc3e4e0a16d
kamillachdev <kamillachdev@gmail.com>
2023-09-24 17:57:14 +0200

    setting values between pictures

--- app/src/main/java/com/example/mappingfloydwarshall/MainActivity.kt
+++ app/src/main/java/com/example/mappingfloydwarshall/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.example.mappingfloydwarshall
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var seekBarProgress = 0
+    var selectedFirstButton: Button? = null
+    var selectedSecondButton: Button? = null
+
+    data class ArrowConnection(
+        val button1: Button,
+        val button2: Button,
+        val textView: TextView
+    )
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var builder = AlertDialog.Builder(this)
+        //ARROWCONNECTIONS INITIALIZATION
+        val arrowConnections = mutableListOf(
+            ArrowConnection(findViewById(R.id.forestButton), findViewById(R.id.swampsButton), findViewById(R.id.forestSwampsText)),
+            ArrowConnection(findViewById(R.id.forestButton), findViewById(R.id.mountainsButton), findViewById(R.id.forestMountainsText)),
+            ArrowConnection(findViewById(R.id.forestButton), findViewById(R.id.caveButton), findViewById(R.id.forestCaveText)),
+            ArrowConnection(findViewById(R.id.mountainsButton), findViewById(R.id.swampsButton), findViewById(R.id.mountainsSwampsText)),
+            ArrowConnection(findViewById(R.id.mountainsButton), findViewById(R.id.caveButton), findViewById(R.id.mountainsCaveText)),
+            ArrowConnection(findViewById(R.id.mountainsButton), findViewById(R.id.jungleButton), findViewById(R.id.mountainsJungleText)),
+            ArrowConnection(findViewById(R.id.swampsButton), findViewById(R.id.jungleButton), findViewById(R.id.swampsJungleText)),
+            ArrowConnection(findViewById(R.id.caveButton), findViewById(R.id.jungleButton), findViewById(R.id.caveJungleText))
+        )
+
+        //START ALERT
+        val builder = AlertDialog.Builder(this)
         builder.setTitle("OBSŁUGA PROGRAMU")
         builder.setMessage("1. Naciśnij jeden z obrazków\n2. Wybierz wagę za pomocą slider'a\n3. Naciśnij inny obrazek, który jest połączony strzałką z poprzednio klikniętym obrazkiem\n4. Powtarzaj poprzednie 3 kroki aby ustalić wiecej wag(opcjonalne)\n5. Naciśnij przycisk 'WYZNACZ TRASĘ' aby wyznaczyć najkrótszą trasę - podświetli się ona na zielono\n6. Naciśnij przycisk 'RESET WAGI' aby wyzerować wszystkie ustawione wagi(opcjonalne)")
         builder.setCancelable(false)
@@ -22,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         val alertDialog = builder.create()
         alertDialog.show()
 
+        //SEEKBAR USABILITY
         val weightText = findViewById<TextView>(R.id.weightText)
         val seek = findViewById<SeekBar>(R.id.weightSeekBar)
 
@@ -32,6 +59,8 @@ class MainActivity : AppCompatActivity() {
                 progress: Int, fromUser: Boolean
             ) {
                 weightText.text = progress.toString()
+                seekBarProgress = progress
+
             }
 
             override fun onStartTrackingTouch(seek: SeekBar?) {
@@ -39,8 +68,29 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(seek: SeekBar?) {
-
             }
         })
+
+        //PICTURES BUTTONS USABILITY
+        val buttonsArray: Array<Button> = arrayOf(findViewById<Button>(R.id.forestButton), findViewById<Button>(R.id.swampsButton), findViewById<Button>(R.id.mountainsButton), findViewById<Button>(R.id.caveButton), findViewById<Button>(R.id.jungleButton))
+        buttonsArray.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                if(selectedFirstButton == null) {
+                    selectedFirstButton = button
+                }
+                else if(selectedSecondButton == null) {
+                    selectedSecondButton = button
+
+                    val arrowConnection = arrowConnections.find {
+                        (it.button1 == selectedFirstButton && it.button2 == selectedSecondButton) || (it.button1 == selectedSecondButton && it.button2 == selectedFirstButton)
+                    }
+
+                    arrowConnection?.textView?.text = seekBarProgress.toString()
+
+                    selectedFirstButton = null
+                    selectedSecondButton = null
+                }
+            }
+        }
     }
 }
